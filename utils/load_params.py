ba368ea6f70cf077fe0b6a1f860bb1f70a3db457
Resul Emre AYGAN <remreaygan@gmail.com>
2023-02-14 00:41:58 +0300

    added load_params.py

---
+++ utils/load_params.py
@@ -0,0 +1,31 @@
+"""
+Author: Resul Emre AYGAN
+"""
+
+import os
+from sys import exit
+from json import load
+
+
+def load_config():
+    try:
+        if not os.path.exists("config.json"):
+            print(f"Konfig dosyasi bulunamadi!")
+            exit()
+
+        with open("config.json") as json_data_file:
+            data = load(json_data_file)
+
+        save_as_png = data["save_as_png"]
+        output_dir = data["output_dir"]
+        crop_size_x = data["crop_size_x"]
+        crop_size_y = data["crop_size_y"]
+        crop_shape = data["crop_shape"]
+        shape_path = data["shape_path"]
+        raster_format = data["raster_format"]
+        raster_path = data["raster_path"]
+
+        return [save_as_png, output_dir, crop_size_x, crop_size_y, crop_shape, shape_path, raster_format, raster_path]
+    except Exception as error:
+        print(f"Konfig dosyasi yuklenirken hata olustu! - Hata: {error}")
+        exit()
