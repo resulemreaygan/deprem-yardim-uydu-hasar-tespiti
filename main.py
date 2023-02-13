0d240f17fc3a0c7df53b91f78a85a212791ed1cf
Resul Emre AYGAN <remreaygan@gmail.com>
2023-02-14 00:43:21 +0300

    added support for clipping shapefile based on raster size

--- main.py
+++ main.py
@@ -3,18 +3,17 @@ Author: Resul Emre AYGAN
 """
 
 import os
-import sys
-import json
-from osgeo import gdal, ogr, osr
+from sys import exit
+from osgeo import gdal
 import numpy as np
-import math
+from math import ceil
 import matplotlib.pyplot as plt
 from uuid import uuid4
 
-from osgeo.osr import OAMS_TRADITIONAL_GIS_ORDER
-from osgeo.gdal import __version__ as osgeo_version
-from shapely import wkt
-from shapely.geometry import Polygon, MultiPolygon
+from utils.load_params import load_config
+from geometry_operations import bounds_to_polygon, transform_polygon_osr, check_epsg, lon_lat_to_geom, \
+    create_shapefile, clip_shapefile_with_shapefile
+from raster_operations import crop_raster_with_warp, crop_raster_with_translate, change_raster_projection
 
 
 def get_array_from_raster(file_path, only_info=True):
@@ -38,15 +37,9 @@ def get_array_from_raster(file_path, only_info=True):
     lon = [min_x, max_x, max_x, min_x]  # [ulx, lrx, lrx, ulx]
     lat = [max_y, max_y, min_y, min_y]  # [uly, uly, lry, lry]
 
-    utm_sr = osr.SpatialReference(wkt=projection)
-    epsg = int(utm_sr.GetAttrValue('AUTHORITY', 1))
+    epsg = check_epsg(projection=projection)
 
-    geom_str = "POLYGON (("
-    for index in range(len(lon)):
-        geom_str += str(lon[index]) + " " + str(lat[index]) + ","
-    geom_str += str(lon[0]) + " " + str(lat[0]) + " ))"
-
-    geom_poly = wkt.loads(geom_str)
+    geom_poly = lon_lat_to_geom(lon=lon, lat=lat)
 
     alpha_channel = None
 
@@ -70,73 +63,6 @@ def get_array_from_raster(file_path, only_info=True):
         temp_ds = None
 
         return geo_transform, min_x, max_y, res_x, res_y, width, height, epsg, geom_poly
-
-
-def crop_raster_with_translate(raster_path, output_path, res_x, res_y, output_bounds, raster_format):
-    try:
-        temp_ds = gdal.Translate(output_path, raster_path, projWin=output_bounds,
-                                 xRes=res_x, yRes=-res_y, outputType=gdal.gdalconst.GDT_Byte, format=raster_format)
-        temp_ds = None
-    except Exception as error:
-        print(f"Raster kesme metotunda hata olustu: {error} - {raster_path}")
-
-
-def crop_raster_with_warp(raster_path, shape_path=None, output_bounds=None, alpha_info=False, output_path=None,
-                          width=0, height=0, cutline_bool=False, epsg_number=4326, multi=False, num_thread=1):
-    try:
-        wo_string = []
-
-        if epsg_number == 4326:
-            epsg_str = "EPSG:4326"
-        else:
-            epsg_str = "EPSG:3857"
-
-        if shape_path is not None:
-            wo_string.append("CUTLINE_ALL_TOUCHED=TRUE")
-
-        if num_thread != 1:
-            wo_string.append("NUM_THREADS=ALL_CPUS")
-
-        _ = gdal.Warp(output_path, raster_path, width=width, height=height, dstSRS=epsg_str,
-                      cutlineDSName=shape_path, cropToCutline=cutline_bool, outputBounds=output_bounds,
-                      resampleAlg=gdal.GRA_Bilinear, dstAlpha=(not alpha_info),
-                      warpOptions=wo_string, multithread=multi)
-        _ = None
-
-        return output_path
-
-    except Exception as error:
-        print(f"Raster kesme metotunda hata olustu: {error} - {raster_path}")
-
-
-def transform_polygon_osr(polygon, src_epsg=4326, dst_epsg=3857):
-    try:
-        if isinstance(polygon, Polygon) or isinstance(polygon, MultiPolygon):
-            polygon = ogr.CreateGeometryFromWkt(polygon.wkt)
-
-        if not isinstance(polygon, ogr.Geometry):
-            print("Geometri ogr geometri formatinda degil!")
-            return False
-
-        src = osr.SpatialReference()
-        tgt = osr.SpatialReference()
-
-        src.ImportFromEPSG(src_epsg)
-        tgt.ImportFromEPSG(dst_epsg)
-
-        if int(osgeo_version[0]) >= 3:
-            # GDAL 3 changes axis order: https://github.com/OSGeo/gdal/issues/1546
-            src.SetAxisMappingStrategy(OAMS_TRADITIONAL_GIS_ORDER)
-            tgt.SetAxisMappingStrategy(OAMS_TRADITIONAL_GIS_ORDER)
-
-        transformer = osr.CoordinateTransformation(src, tgt)
-
-        polygon.Transform(transformer)
-
-        return wkt.loads(polygon.ExportToWkt())
-
-    except Exception as error:
-        print(f"transform_polygon_osr isleminde hata olustu: {error}")
 
 
 def normalize_byte(raster_array):
@@ -169,48 +95,41 @@ def save_raster_as_png(raster_array, output_path, generate_alpha, normalize=True
     return output_path
 
 
-def bounds_to_polygon(geom_bounds):
-    return Polygon([(geom_bounds[0], geom_bounds[1]), (geom_bounds[0], geom_bounds[3]),
-                    (geom_bounds[2], geom_bounds[3]), (geom_bounds[2], geom_bounds[1])])
-
-
 if __name__ == '__main__':
-    if not os.path.exists("config.json"):
-        print("Konfig dosyasi bulunamadi!\n")
-        sys.exit()
-
-    with open("config.json") as json_data_file:
-        data = json.load(json_data_file)
-
-    save_as_png = data["save_as_png"]
-    output_dir = data["output_dir"]
-    crop_size_x = data["crop_size_x"]
-    crop_size_y = data["crop_size_y"]
-
+    [save_as_png, output_dir, crop_size_x, crop_size_y, crop_shape, shape_path, raster_format, raster_path] = \
+        load_config()
     use_warp = False
-    raster_format = data["raster_format"]
 
-    raster_path = data["raster_path"]
+    if not os.path.exists(raster_path):
+        print(f"Raster bulunamadi! - {raster_path}")
+        exit()
 
-    raster_path_4326 = os.path.join(os.path.split(raster_path)[0], str(uuid4()) + '.tif')
+    raster_path_4326 = os.path.join(output_dir, str(uuid4()) + '.tif')
 
     [geo_transform, x_min, y_max, res_x, res_y, width, height, epsg, geom_poly] = get_array_from_raster(
         file_path=raster_path)
+    #
+    # crop_size_x = min(width, crop_size_x)
+    # crop_size_y = min(height, crop_size_x)
 
     generate_alpha = False
 
     if epsg != 4326:
+        print(f"Raster EPSG:4326 formatinda degil, donusturuluyor. - {raster_path}")
         generate_alpha = True
-        gdal.Warp(raster_path_4326, raster_path, srcSRS="EPSG:" + str(epsg), dstSRS="EPSG:4326", dstAlpha=True)
+
+        if not change_raster_projection(raster_path=raster_path, output_path=raster_path_4326, src_epsg=epsg,
+                                        dst_epsg=4326):
+            exit()
 
         [geo_transform, x_min, y_max, res_x, res_y, width, height, epsg, geom_poly] = get_array_from_raster(
             file_path=raster_path_4326)
         raster_path = raster_path_4326
 
     x_not_round = width / crop_size_x
-    x_round = math.ceil(x_not_round)
+    x_round = ceil(x_not_round)
     y_not_round = height / crop_size_y
-    y_round = math.ceil(y_not_round)
+    y_round = ceil(y_not_round)
 
     pix_to_mx = crop_size_x * x_round * res_x
     pix_to_my = crop_size_y * y_round * abs(res_y)
@@ -220,6 +139,11 @@ if __name__ == '__main__':
 
     x_steps = [x_min + x_size * i for i in range(x_round + 1)]
     y_steps = [y_max - y_size * i for i in range(y_round + 1)]
+
+    if crop_shape:
+        if not os.path.exists(shape_path):
+            print(f"Shapefile bulunamadi! - {shape_path}")
+            crop_shape = False
 
     for i in range(x_round):
         for j in range(y_round):
@@ -251,6 +175,33 @@ if __name__ == '__main__':
             if save_as_png:
                 save_raster_as_png(raster_array=original_raster, alpha_channel=alpha_channel,
                                    output_path=temp_output_path_png, generate_alpha=generate_alpha)
+
+            if crop_shape:
+                temp_shape_path = os.path.join(output_dir, (str("01") + "-" + str(j) + "-" + str(i) + "_tmp.shp"))
+                temp_shx_path = os.path.join(output_dir, (str("01") + "-" + str(j) + "-" + str(i) + "_tmp.shx"))
+                temp_dbf_path = os.path.join(output_dir, (str("01") + "-" + str(j) + "-" + str(i) + "_tmp.dbf"))
+                temp_prj_path = os.path.join(output_dir, (str("01") + "-" + str(j) + "-" + str(i) + "_tmp.prj"))
+                temp_cfg_path = os.path.join(output_dir, (str("01") + "-" + str(j) + "-" + str(i) + "_tmp.cfg"))
+
+                crop_shape_path = os.path.join(output_dir, (str("01") + "-" + str(j) + "-" + str(i) + ".shp"))
+
+                if create_shapefile(geom_wkt=geom_poly.wkt, output_path=temp_shape_path, epsg=epsg):
+                    if not clip_shapefile_with_shapefile(input_shapefile=shape_path, clip_shapefile=temp_shape_path,
+                                                         output_shapefile=crop_shape_path):
+                        if os.path.exists(crop_shape_path):
+                            os.remove(crop_shape_path)
+
+                if os.path.exists(temp_shape_path):
+                    os.remove(temp_shape_path)
+                if os.path.exists(temp_shx_path):
+                    os.remove(temp_shx_path)
+                if os.path.exists(temp_dbf_path):
+                    os.remove(temp_dbf_path)
+                if os.path.exists(temp_prj_path):
+                    os.remove(temp_prj_path)
+                if os.path.exists(temp_cfg_path):
+                    os.remove(temp_cfg_path)
+
     ds = None
 
     if os.path.exists(raster_path_4326):
