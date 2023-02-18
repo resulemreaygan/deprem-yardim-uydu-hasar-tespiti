0993f4c8213199115cbc7b216fbe5c1bfa508a4e
Resul Emre AYGAN <remreaygan@gmail.com>
2023-02-18 16:33:49 +0300

    deleted unused func

--- utils/file_operations.py
+++ utils/file_operations.py
@@ -31,13 +31,6 @@ def get_file_name(file_path):
     return os.path.split(file_path)[1].split('.')[0]
 
 
-def generate_temp_raster_path(output_path, raster_name=''):
-    if raster_name == '':
-        raster_name = str(uuid4())
-
-    return os.path.join(output_path, raster_name + '.tif')
-
-
 def generate_temp_file_path(output_path, file_ext, file_name=''):
     if file_name == '':
         file_name = str(uuid4())
